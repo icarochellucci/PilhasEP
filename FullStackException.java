8e5c5c5a75843f0f10d9f0b15f74f9d1c0051cfe
Icaro Chellucci <icaro.chellucci00@gmail.com>
2020-10-12 13:33:49 -0300

    Add files via upload

---
+++ FullStackException.java
@@ -0,0 +1,4 @@
+  
+public class FullStackException extends RuntimeException {
+  public FullStackException(String err){ super(err); }
+}
---
+++ NodeStack.java
@@ -0,0 +1,34 @@
+public class NodeStack<E> implements Stack<E> {
+  protected Node<E> top;
+  protected int size;
+
+  public NodeStack(){
+    top = null;
+    size = 0;
+  }
+
+  public int size(){ return size; }
+
+  public boolean isEmpty(){ return top == null; }
+
+  public E top() throws EmptyStackException {
+    if(isEmpty()) throw new EmptyStackException("Pilha vazia");
+    return top.getElement();
+  }
+
+  public void push(E elem){
+    Node<E> newNode = new Node<E>(elem, top);
+    top = newNode;
+    size++;
+  }
+
+  public E pop() throws EmptyStackException {
+    if(isEmpty()) throw new EmptyStackException("Pilha vazia");
+    E tmp = top.getElement();
+    top = top.getNext();
+    size--;
+    return tmp;
+  }
+
+  // representação da pilha como uma string em ordem reversa
+  }
